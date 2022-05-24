3c5ec8156acd32b7a3718baa88e888055dd3eb5f
Nathan Healea <nate.healea@gmail.com>
2022-05-24 11:02:39 -0700

    Add Tailwindcss intergration, and boilerplate landing page

--- Auth0.Client/src/App.tsx
+++ Auth0.Client/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import packageJson from '../package.json';
+
 function App() {
   return (
-        <h1>Auth0 Boilerplate</h1>
+    <div className=' h-screen flex flex-col justify-center items-center'>
+      <div className=' grow flex flex-col justify-center items-center'>
+        <h1 className=' text-4xl uppercase font-mono font-thin'>
+          {packageJson.name}
+        </h1>
+        <h2 className='text-xl'>{packageJson.description}</h2>
+      </div>
+      <p>v{packageJson.version}</p>
+    </div>
   );
 }
 
